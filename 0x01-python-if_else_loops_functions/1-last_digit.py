8ad57af401ffe914bcb35f78884dd54345cd375d
Akanni Feyisara <Creativeslides@outlook.com>
2022-12-04 01:07:06 +0000

    write for loop programs in python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if (number < 0):
 	last_digit = number % 10
 	number = -number
 	last_digit = -last_digit
-elif (number > 0):
+else:
 	last_digit = number % 10
 if (last_digit > 5):
 	print('Last digit of', number, 'is', last_digit, 'and is greater than 5')
