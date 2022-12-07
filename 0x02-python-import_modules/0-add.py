607c6b5615950bd39dbe819af45ac9bd8bea4041
Akanni Feyisara <creativeslides@outlook.com>
2022-12-07 18:11:27 +0100

    write a program that imports a function from a module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-
-    add_func = add(a, b)
-
-    print("{:d} + {:d} = {:d}".format(a, b, add_func))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
