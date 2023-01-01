7ec776bb8b18e42cd826ba00dc20c5085abd3c1f
Akanni Feyisara <creativeslides@outlook.com>
2023-01-01 08:54:00 -0800

    PyTHON Data structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     return ([list(map(lambda x: x * x, row)) for row in matrix])
+
+#MyCode
+'''def square_matrix_simple(matrix=[]):
+    new_list = []
+   # inner_list = []
+    for row in matrix:
+        inner_list = []
+        for col in range(len(row)):
+            inner_list.append(row[col] ** 2)
+        new_list.append(inner_list)
+    return new_list
+'''
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
-
-        sorted_dic_array = (sorted(a_dictionary.keys()))
-
-            for key in sorted_dic_array:
-                        print("{:s}: {}".format(key, a_dictionary[key]))
+    sorted_dict = list(sorted(a_dictionary.keys()))
+    for key in sorted_dict:
+        print("{}: {}".format(key, a_dictionary[key]))
