dfaab231f93377b9a8bd7543d2cdf71d87aa8bcd
Akanni Feyisara <creativeslides@outlook.com>
2023-01-02 06:21:51 -0800

    Write a function that prints string characters

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This module contains a afunction that
+prints string characters to stdout
+'''
+def say_my_name(first_name, last_name=""):
+    '''
+    say_my_name is a function that prints the first and
+    last names of users in string format
+    '''
+    if not type(first_name) is str:
+        raise TypeError("first_name must be a string")
+    if not type(last_name) is str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
