b8fac28a56e613d7470e0721c97e0c55b2a27162
Akanni Feyisara <creativeslides@outlook.com>
2023-01-04 08:34:06 -0800

    Classes and objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 0-Rectangle
+Contains a class Rectangle
+Makes an empty Rectangle class
+"""
+
+
+class Rectangle(object):
+    """
+    This class creates an instance of a Rectangle class
+    It creates an empty Rectangle object
+    """
+
+    pass
