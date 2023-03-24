d85de175ff5a3086b1a4173539f5e12ad5804f18
Akanni Feyisara <akannifeyisara6@gmail.com>
2023-03-24 13:20:53 +0100

    Fix error code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Create a square class with a size value"""
-
-
 class Square:
-    """Create a size value"""
+    """
+        This class create a square with private value size
+    """
     def __init__(self, size):
-        self.__size = 
+        """This method initializes the private attribute, size"""
+        self.__size = size
