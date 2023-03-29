c1b12a446dba1f604d4ebcf58d722b29a8685f2e
Akanni Feyisara <akannifeyisara6@gmail.com>
2023-03-29 19:46:08 +0100

    Solve the bytecode task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import math
+"""
+    Bytecode module to calculate the area
+    and circumference of a circle
+"""
+
+
+class MagicClass:
+    """
+        Creates magic class
+    """
+    def __init__(self, radius=0):
+        """
+            instantiate circle attribute
+        """
+        self.__radius = 0
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """
+            calculates area of the circle
+        """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """
+            calculates the circumference of the circle
+        """
+        return 2 * math.pi * self.__radius
