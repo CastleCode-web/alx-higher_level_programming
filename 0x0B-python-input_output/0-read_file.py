a49eac69f90f70d4c16afa6bed605c995ae676fd
Akanni Feyisara <akannifeyisara6@gmail.com>
2023-04-18 20:05:06 +0100

    Python Input and Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@
 
 def read_file(filename=""):
     """ reads a text file and prints it to stdout """
-    with open(filename, encoding="utf-8") as MyFile:
+    with open(filename, mode = "r", encoding="utf-8") as MyFile:
         print(MyFile.read(), end="")
