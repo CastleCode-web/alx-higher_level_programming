f25796e25cbcee5cfe9d9370be159d7e08fd2b47
Akanni Feyisara <akannifeyisara6@gmail.com>
2023-04-23 17:39:21 +0100

    create a base class and a unittest for it

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module contains a class with a private class attribute,
+    amd class constructor
+"""
+
+
+class Base(object):
+    """ creates a class named Base that inherits from object
+        superclass
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ Base class constructor that accept id as the only argument
+            and sets it to none
+        """
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module contains unittest cases for base.py """
+import unittest
+Base = __import__("base").Base
+
+
+class TestBase(unittest.TestCase):
+    """ creates a test class for the base.py file """
+    def test_initial_value(self):
+        """ tests the init constructor """
+        obj_1 = Base()
+        self.assertEqual(obj_1.id, 1)
+        self.assertIsNotNone(obj_1.id)
+        self.assertNotEqual(obj_1.id, 0)
+        
+        obj_2 = Base(12)
+        self.assertNotEqual(obj_2.id, 1)
+        self.assertEqual(obj_2.id, 12)
+        self.assertIsNotNone(obj_2.id)
