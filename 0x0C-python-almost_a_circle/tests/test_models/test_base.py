c958728eecd1715402acaba3189b156e8fca6284
Akanni Feyisara <akannifeyisara6@gmail.com>
2023-04-23 17:53:38 +0100

    create a base class and a unittest for it

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,3 +17,8 @@ class TestBase(unittest.TestCase):
         self.assertNotEqual(obj_2.id, 1)
         self.assertEqual(obj_2.id, 12)
         self.assertIsNotNone(obj_2.id)
+
+        obj_3 = Base()
+        self.assertEqual(obj_3.id, 2)
+        self.assertIsNotNone(obj_3.id)
+        self.assertNotEqual(obj_3.id, 1)
